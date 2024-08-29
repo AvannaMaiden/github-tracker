320059f3e1229a26cd9a471b4a2675c2519fddba
AvannaMaiden <f29stb@yandex.ru>
2024-08-30 00:17:25 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import http.client
+import json
+
+def github_activity(username):
+    connection = http.client.HTTPSConnection("api.github.com")
+
+    header = {
+    'User-Agent': "PythonApp"
+    }
+
+    connection.request("GET", f"/users/{username}/events", headers=header)
+    response = connection.getresponse()
+    data = response.read().decode('utf-8')
+    connection.close()
+
+    print(f"User: {username}")
+
+    if response.status == 404:
+        print(f"Username {username} not found")
+    elif response.status != 200:
+        print(f"Error: {response.status}")
+
+    events = json.loads(data)
+    if not events:
+        print("No user activity")
+        return
+    
+    for event in events:
+        event_type = event.get("type")
+        repo_name = event.get("repo",{}).get("name")
+        created_at = event.get("created_at")
+        print(f"{created_at}: {event_type} in {repo_name}")
+
+if __name__ == "__main__":
+    username = input("Enter the GitHub username: ")
+    print('-'*40)
+    github_activity(username)
+    print('-'*40)
---
+++ readme.md
@@ -0,0 +1,4 @@
+How to use:
+1. Choose path to tracker.py 
+2. python tracker.py
+3. enter the username"# github-tracker" 
